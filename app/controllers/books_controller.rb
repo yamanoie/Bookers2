ca2f16d343e3b6fe885ddb1a0c7d6fa12d412cbd
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-06-24 13:16:28 +0000

    user info finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,26 @@
 class BooksController < ApplicationController
-	def new
-	end
 	def create
+		book = Book.new(book_params)
+		book.user_id = current_user.id
+		book.save
+		redirect_to books_path
 	end
 	def index
+		@books = Book.all
+		@book = Book.new
+		@user = current_user
 	end
 	def show
+		@book = Book.find(params[:id])
+		@book_new =Book.new
 	end
 	def edit
 	end
 	def destroy
+	end
+
+private
+	def book_params
+		params.require(:book).permit(:title, :body)
 	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,12 @@ class UsersController < ApplicationController
 	end
 	def index
 		@users = User.all
+		@book = Book.new
+		@user = current_user
 	end
 	def show
 		@user = User.find(params[:id])
+		@book = Book.new
 	end
 	def edit
 	end
